e838382281ee6af8a996530555690a56655bda0a
tatiana sigal <tatiana.sigal@bue.edu.ar>
2022-06-19 18:25:06 -0300

    js

---
+++ main.js
@@ -0,0 +1,14 @@
+let nombre = prompt("Pone tu nombre");
+let numeroUser = alert("Pensá un número, pero no me lo digas");
+let numero1User = alert("Sumale 5");
+let numero2User = alert("Multiplicalo por 2");
+let numero3User = alert("Resta 4");
+let numero4User = alert("Dividilo por 2");
+let numero5User = alert("Restale el número que pensaste al principio");
+let numero1 = 5;
+let numero2 = 2;
+let numero3 = 4;
+let numero4 = 2;
+let resultado = (numero1 * numero2 - numero3) / numero4;
+let resultadoUser = alert("Te quedó " + resultado);
+let nombreUser = alert("Gracias por jugar " + nombre);
