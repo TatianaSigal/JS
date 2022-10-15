e5eff367664dedecd103e04994c7a963fce16865
TatianaSigal <tatiana.sigal@bue.edu.ar>
2022-10-15 18:29:17 -0300

    nuevo curso JS

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-let nombre = prompt("Pone tu nombre");
-let numeroUser = alert("Pensá un número, pero no me lo digas");
-let numero1User = alert("Sumale 5");
-let numero2User = alert("Multiplicalo por 2");
-let numero3User = alert("Resta 4");
-let numero4User = alert("Dividilo por 2");
-let numero5User = alert("Restale el número que pensaste al principio");
-let numero1 = 5;
-let numero2 = 2;
-let numero3 = 4;
-let numero4 = 2;
-let resultado = (numero1 * numero2 - numero3) / numero4;
-let resultadoUser = alert("Te quedó " + resultado);
-let nombreUser = alert("Gracias por jugar " + nombre);
+let nombre = prompt("escribe tu nombre")
+let apellido = prompt("ingresa tu apellido");
+let edad = parseInt(prompt("ingresa tu edad"));
+
+let salida ="Bienvenido"+" "+ nombre +" "+ apellido +" "+ edad;
+alert (salida);
+
+// LAS TABLAS 
+
+let ingresaNumero = parseInt (prompt("Ingresa un Numero del 1 al 10"))
+for(let i = 0 ; i <= 10 ; i++){
+    let resultado = ingresaNumero * i;
+    console.log( ingresaNumero + "x"+ i +" "+ "=" +" "+ resultado);
+
+}
+
